d16c8f90fabcea46db2329606992549e5fd9f800
Iris Ng <irisngwl@gmail.com>
2019-03-11 15:45:25 +0800

    Add async await

--- client/src/actions/seedHelperFunctions.js
+++ client/src/actions/seedHelperFunctions.js
@@ -11,7 +11,7 @@ import productCategories from '../components/NavigationBar/productCategories';
 //93379ab06bfd41ea4b27440a1e2d7d76c1ea3d91245b7952b17222f8f7d281bb
 //5a0c866a06a1917ee16b8299423796e7a6a2184a73f828f483f81704a013cad8
 
-export const loopSeedShop = (flattenedCategories, numberOfSeed, ownerId) => {
+export const loopSeedShop = async (flattenedCategories, numberOfSeed, ownerId) => {
    var createdShops = [];
    //Number of rounds to seed categories
    for (let i = 1; i <= numberOfSeed; i++) {
@@ -21,19 +21,19 @@ export const loopSeedShop = (flattenedCategories, numberOfSeed, ownerId) => {
          var createdShop;
          if (i < 20) {
             //Limited by Unsplash api to only be able to make 50 requests per api key
-            createdShop = seedShop(category, ownerId, '241dc288805f06bd42fa533605ecee15d51efcbee48ed56795546b7ad260a22e');
+            createdShop = await seedShop(category, ownerId, '241dc288805f06bd42fa533605ecee15d51efcbee48ed56795546b7ad260a22e');
          } else if (i < 40) {
-            createdShop = seedShop(category, ownerId, '00723b1f2243e6f09ab1e1c672d41b226aa3a1f9d925e66b985b6b4f1af6f7f9');
+            createdShop = await seedShop(category, ownerId, '00723b1f2243e6f09ab1e1c672d41b226aa3a1f9d925e66b985b6b4f1af6f7f9');
          } else if (i < 60) {
-            createdShop = seedShop(category, ownerId, '31d69f18309f38a06f4ba0bdd80b02d8923e9fb8346abfa86fa62c7a87090ca4');
+            createdShop = await seedShop(category, ownerId, '31d69f18309f38a06f4ba0bdd80b02d8923e9fb8346abfa86fa62c7a87090ca4');
          } else if (i < 80) {
-            createdShop = seedShop(category, ownerId, '74cb2ec52ba0abd5742b18e3bd605e6234adb06575e0032c68ec2843b1201e71');
+            createdShop = await seedShop(category, ownerId, '74cb2ec52ba0abd5742b18e3bd605e6234adb06575e0032c68ec2843b1201e71');
          } else if (i < 100) {
-            createdShop = seedShop(category, ownerId, '03c1a7de3a21646c174a65ac83e63305bf0218c3db98f535f56beea157bf2b22');
+            createdShop = await seedShop(category, ownerId, '03c1a7de3a21646c174a65ac83e63305bf0218c3db98f535f56beea157bf2b22');
          } else if (i < 120) {
-            createdShop = seedShop(category, ownerId, '93379ab06bfd41ea4b27440a1e2d7d76c1ea3d91245b7952b17222f8f7d281bb');
+            createdShop = await seedShop(category, ownerId, '93379ab06bfd41ea4b27440a1e2d7d76c1ea3d91245b7952b17222f8f7d281bb');
          } else {
-            createdShop = seedShop(category, ownerId, '5a0c866a06a1917ee16b8299423796e7a6a2184a73f828f483f81704a013cad8');
+            createdShop = await seedShop(category, ownerId, '5a0c866a06a1917ee16b8299423796e7a6a2184a73f828f483f81704a013cad8');
          }
          createdShops = [...createdShops, createdShop.data];
       }
