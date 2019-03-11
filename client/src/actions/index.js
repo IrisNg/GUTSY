b58bcf396d7473a49aabd17807e00cc1a8dcddab
Iris Ng <irisngwl@gmail.com>
2019-03-11 15:51:30 +0800

    Fix action creator bug of importing a invalid function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import history from '../history';
-import { loopCreateShop, flattenCategories, getNumberOfSeedRounds } from './seedHelperFunctions';
+import { loopSeedShop, flattenCategories, getNumberOfSeedRounds } from './seedHelperFunctions';
 
 export const selectCategory = (name, param) => {
    return {
